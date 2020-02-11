adf0778532d2e98d887d4d19787327de1b6925ac
AndreyGrey <andreyskakunenko@gmail.com>
2020-02-12 00:55:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/surfaceviewexample/FlashlightCone.java
@@ -0,0 +1,30 @@
+package com.example.surfaceviewexample;
+
+public class FlashlightCone {
+    private int mX;
+    private int mY;
+    private int mRadius;
+
+    public FlashlightCone(int viewWidth, int viewHeight) {
+        mX = viewWidth / 2;
+        mY = viewHeight / 2;
+        // Adjust the radius for the narrowest view dimension.
+        mRadius = ((viewWidth <= viewHeight) ? mX / 3 : mY / 3);
+    }
+
+    public int getmX() {
+        return mX;
+    }
+
+    public int getmY() {
+        return mY;
+    }
+
+    public int getmRadius() {
+        return mRadius;
+    }
+    public void update(int newX, int newY) {
+        mX = newX;
+        mY = newY;
+    }
+}
